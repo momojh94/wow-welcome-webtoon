8e9dcafbe2fe75fb01a0564ef493086582d94a96
momo <mjh79017901@gmail.com>
2021-04-14 03:13:49 +0900

    chore: spring-restdocs 모듈 추가
    일단 platform server만 적용해보고 다른 곳도 추가할 예정

---
+++ backend/platform_server/src/test/java/com/www/platform/controller/CommentsControllerTest.java
@@ -0,0 +1,10 @@
+package com.www.platform.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@ExtendWith(RestDocumentationExtension.class)
+public class CommentsControllerTest {
+
+}
